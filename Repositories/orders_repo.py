f5ede99b14b7c7966d14cc4eb590496c6cd188b0
dorbi <dor.bilia2@gmila.com>
2026-03-12 19:38:20 +0200

    moved producing responsibility to service

--- Repositories/orders_repo.py
+++ Repositories/orders_repo.py
@@ -6,15 +6,9 @@ from Repositories.Users_repo import UsersRepo
 class OrdersRepo:
     def __init__(self):
         self.conn = DBHandler()
-        config = {'bootstrap.servers': 'localhost:9092'}
-        self.producer = OrderProducer(config)  # to change producer placement
         self.users = UsersRepo()
 
-    def make_order(self, item_id, item_name, username, quantity):
+    def add(self, user_id, item_id, quantity):
         sql = """INSERT INTO orders (user_id, item_id, quantity) VALUES
         (%s,%s,%s)"""
-        # user_id = self.users.get_user_id_by_name(username)
-        self.conn.execute(sql, params=(3, item_id, quantity))
-        order = {'item_id': item_id, 'quantity': quantity, 'product': item_name,
-                 'buyer': username}
-        self.producer.publish('order', order)
+        self.conn.execute(sql, params=(user_id, item_id, quantity))
