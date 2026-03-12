2932600d27ded1e6fd3736e2ac811e6b351cbf67
dorbi <dor.bilia2@gmila.com>
2026-03-12 19:38:44 +0200

    changed main to match previous changes

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from Repositories.Items_repo import itemsRepo
-from Repositories.orders_repo import OrdersRepo
-
-
-# from Consumers.notifier_consumer import Notifier
-# from Consumers.update_stock_consumer import StockUpdate
-
+from Services.orders_service import OrdersService
+from Repositories.Users_repo import UsersRepo
 
 def display_items(items):
-    index = 1
     print("Nm.| Item            | Price     |In Stock")
     print("-----------------")
     for row in items:
@@ -16,24 +11,24 @@ def display_items(items):
         price = row['item_price']
         stock = row['item_stock']
         print(f"{itemId}. {name:<10}        : {price:<10}: {stock:<10}")
-        index += 1
 
 
 class KafkaDemo:
     def __init__(self):
+        self.users = UsersRepo()
         self.itemRepo = itemsRepo()
-        self.ordersRepo = OrdersRepo()
+        self.ordersService = OrdersService()
 
     def start(self):
         username = input("Enter your username:")
+        user_id = self.users.get_user_id_by_name(username)
         while True:
             items = self.itemRepo.get_items()
             display_items(items)
             item = int(input("Enter item number: "))
             quantity = int(input("Enter quantity: "))
             item_id = items[item - 1]['item_id']
-            item_name = items[item - 1]['item_name']
-            self.ordersRepo.make_order(item_id, item_name, username, quantity)
+            self.ordersService.make_order(item_id, user_id, quantity)
 
 
 if __name__ == "__main__":
