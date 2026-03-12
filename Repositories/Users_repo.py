421b55ee6e4278380b416d80ff5b36c3065e2032
dorbi <dor.bilia2@gmila.com>
2026-03-12 19:36:47 +0200

    fixed get by id

--- Repositories/Users_repo.py
+++ Repositories/Users_repo.py
@@ -6,5 +6,5 @@ class UsersRepo:
         self.conn = DBHandler()
 
     def get_user_id_by_name(self, user_name):  # to be changed
-        sql = """SELECT id FROM users WHERE username = %s"""
-        self.conn.execute(sql, params=user_name, fetch=True)
+        sql = f"""SELECT id FROM users WHERE username = %s """
+        return self.conn.execute(sql,(user_name,), fetch=True)[0][0]
